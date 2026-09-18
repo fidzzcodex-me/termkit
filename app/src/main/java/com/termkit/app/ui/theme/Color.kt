92d784f269484984723972284aec58a69f43a9b4
fidzzcodex.me <rinatonidi28@gmail.com>
2026-09-18 12:50:11 +0700

    Upload: add app/src/main/java/com/termkit/app/ui/theme/Color.kt

---
+++ app/src/main/java/com/termkit/app/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.termkit.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BluePrimaryLight = Color(0xFF2F6FED)
+val BluePrimaryDark = Color(0xFF5B8DEF)
+val BackgroundLight = Color(0xFFFAFAFA)
+val SurfaceLight = Color(0xFFFFFFFF)
+val BackgroundDark = Color(0xFF121417)
+val SurfaceDark = Color(0xFF1A1D21)
+val BorderLight = Color(0xFFE2E4E8)
+val BorderDark = Color(0xFF2C2F34)
+val TextPrimaryLight = Color(0xFF16181D)
+val TextPrimaryDark = Color(0xFFF2F3F5)
+val SuccessColor = Color(0xFF1FA463)
+val ErrorColor = Color(0xFFDC4C4C)
+val WarningColor = Color(0xFFD98E2C)
+val InfoColor = Color(0xFF2F6FED)
